6d509d478e99addee5ad73a65548d724762c132b
JimageJ <james.rowe@slcu.cam.ac.uk>
2020-08-08 11:54:50 +0100

    Update Segment_and_FRET_timeseries.py

--- Segment_and_FRET_timeseries.py
+++ Segment_and_FRET_timeseries.py
@@ -429,7 +429,7 @@ stats=StackStatistics(conFRETImp2)
 conFRETImp2 = CompositeImage(conFRETImp2, CompositeImage.COMPOSITE)  
 IJ.setMinAndMax(conFRETImp2, 500, 3000)
 conFRETImp2.show()
-IJ.run("16_color_ramp")
+IJ.run("16_colors")
 	
 
 conFRETProjImp= ImagePlus( "Max Z  projection of emission ratios X1000 of "+ originalTitle, conFRETProjImpStack)
@@ -439,7 +439,7 @@ stats=StackStatistics(conFRETProjImp)
 IJ.setMinAndMax(conFRETProjImp, 500, 3000)
 conFRETProjImp = CompositeImage(conFRETProjImp, CompositeImage.COMPOSITE)  
 conFRETProjImp.show()
-IJ.run("16_color_ramp")
+IJ.run("16_colors")
 	
 conlabelImp= ImagePlus("Label map "+ originalTitle, conlabelImpStack)
 conlabelImp.setDimensions(1, imp1.getNSlices(), imp1.getNFrames())
